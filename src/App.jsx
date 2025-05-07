e5a50e994031b65b91a28cca7a62e5d0228847c3
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-05-08 00:12:10 +0530

    counter code in app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const handleClick = (sign) => {
+    setCount((prevCount) => (sign === '+' ? prevCount + 1 : prevCount - 1));
+  };
+
+  return (
+    <>
+      <h1>Counter App</h1>
+      <div>
+        <button onClick={() => (
+          handleClick('+')
+        )}>Increment</button>
+        <button onClick={()=> (
+          handleClick('-')
+        )}>Decrement</button>
+      </div>
+    </>
+  )
+}
+
+export default App
